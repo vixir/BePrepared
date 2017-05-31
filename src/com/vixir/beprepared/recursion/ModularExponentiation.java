8becd0fbe6a7bced8b9894620abae564b59e7c70
Vivek Kumar Verma <vivk274@gmail.com>
2017-06-01 00:04:00 +0530

    simple recursion

---
+++ src/com/vixir/beprepared/recursion/ModularExponentiation.java
@@ -0,0 +1,23 @@
+package com.vixir.beprepared.recursion;
+
+public class ModularExponentiation {
+
+	// calculate (x^n) % m
+	// (x^n % m) = (x^(n/2)%M)x(x^(n/2)%M)%M if n is even
+	//           = (x%m)*(mod(x,n-1,m))
+	public static void main(String[] args) {
+		System.out.println(mod(5,4,9));
+	}
+
+	public static int mod(int x, int n, int m) {
+		if (n == 0) {
+			return 1;
+		}
+		if (n % 2 == 0) {
+			int y = mod(x, n / 2, m);
+			return (y * y) % m;
+		} else {
+			return (x % m) * (mod(x, n - 1, m)) % m;
+		}
+	}
+}
