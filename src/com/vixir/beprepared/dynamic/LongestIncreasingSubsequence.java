c5bfc9c0f66ad07c148f17eb1d389d3b2378183d
Vivek Kumar Verma <vivk274@gmail.com>
2019-06-14 00:26:15 +0530

    Longest Increasing Sequence nLogN

--- src/com/vixir/beprepared/dynamic/LongestIncreasingSubsequence.java
+++ src/com/vixir/beprepared/dynamic/LongestIncreasingSubsequence.java
@@ -6,30 +6,32 @@ import java.util.stream.*;
 public class LongestIncreasingSubsequence {
 
     public static void main(String[] args) {
-        int a[] = {3, 4, -1, 0, 6, 2, 3};
+        int[] a = {3, 4, -1, 0, 6, 2, 3};
         System.out.println("\n longest increasing subsequence is " + longestIncreasingSubsequence(a));
     }
 
     public static ArrayList<Integer> longestIncreasingSubsequence(int[] a) {
-        int[] lenStore = new int[a.length];
-        // will store the indices of the array which will help to create LongestIncreasingSubsequence
-        int[] indices = new int[a.length + 1];
+        int[] indexArray = new int[a.length];
+        // will store the memArray of the array which will help to create LongestIncreasingSubsequence
+        int[] memArray = new int[a.length + 1];
         //this will store the maximum subsequence length
         int length = 0;
 
-        IntStream.range(0, a.length + 1).forEach(i -> indices[i] = Integer.MIN_VALUE);
+        IntStream.range(0, a.length + 1).forEach(i -> memArray[i] = Integer.MIN_VALUE);
 
         for (int i = 0; i < a.length; i++) {
             //starting from index 1, for recursive binarySearch
-            int updatedLength = binarySearch(1, length, indices, a, a[i]);
-            lenStore[i] = indices[updatedLength - 1];
-            indices[updatedLength] = i;
+            int updatedLength = binarySearch(1, length, memArray, a, a[i]);
+            indexArray[i] = memArray[updatedLength - 1];
+            memArray[updatedLength] = i;
             length = Math.max(length, updatedLength);
         }
         ArrayList<Integer> sol = new ArrayList<>();
-        for (int i = indices[length]; indices[i] != Integer.MIN_VALUE; ) {
-            sol.add(0, a[i]);
-            i = indices[i];
+
+        int currIndex = memArray[length];
+        while (currIndex != Integer.MIN_VALUE) {
+            sol.add(0, a[currIndex]);
+            currIndex = indexArray[currIndex];
         }
         return sol;
     }
@@ -38,7 +40,7 @@ public class LongestIncreasingSubsequence {
         if (left > right) {
             return left;
         }
-        int mid = (left + right) / 2;
+        int mid = left + (right - left) / 2;
         if (a[indices[mid]] < x) {
             left = mid + 1;
         } else {
