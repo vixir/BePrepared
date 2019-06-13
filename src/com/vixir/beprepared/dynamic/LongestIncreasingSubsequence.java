3d607ccd5c0dae6f7fc60f2c5bf01e53437728d8
Vivek Kumar Verma <vivk274@gmail.com>
2019-06-14 00:00:15 +0530

    Longest Increasing Sequence nLogN

--- src/com/vixir/beprepared/dynamic/LongestIncreasingSubsequence.java
+++ src/com/vixir/beprepared/dynamic/LongestIncreasingSubsequence.java
@@ -1,41 +1,50 @@
 package com.vixir.beprepared.dynamic;
 
+import java.util.*;
+import java.util.stream.*;
+
 public class LongestIncreasingSubsequence {
 
-	public static void main(String[] args) {
-		int a[] = { 3, 4, -1, 0, 6, 2, 3 };
-		int length = longestIncreasingSubsequence(a);
-		System.out.println("\n longest increasing subsequence is " + length);
-	}
+    public static void main(String[] args) {
+        int a[] = {3, 4, -1, 0, 6, 2, 3};
+        System.out.println("\n longest increasing subsequence is " + longestIncreasingSubsequence(a));
+    }
 
-	public static int longestIncreasingSubsequence(int[] a) {
-		int j = 0;
-		int i = 0;
-		int length = 0;
-		int ans[] = new int[a.length];
+    public static ArrayList<Integer> longestIncreasingSubsequence(int[] a) {
+        int[] lenStore = new int[a.length];
+        // will store the indices of the array which will help to create LongestIncreasingSubsequence
+        int[] indices = new int[a.length + 1];
+        //this will store the maximum subsequence length
+        int length = 0;
 
-		for (i = 0; i < a.length; i++) {
-			ans[i] = 1;
-		}
+        IntStream.range(0, a.length + 1).forEach(i -> indices[i] = Integer.MIN_VALUE);
 
-		for (i = 1; i < a.length; i++) {
-			while (j < i) {
-				if (a[j] < a[i]) {
-					ans[i] = Math.max(ans[i], ans[j] + 1);
-					if (length < ans[i]) {
-						length = ans[i];
-					}
-				}
-				j++;
-			}
-			j = 0;
-		}
+        for (int i = 0; i < a.length; i++) {
+            //starting from index 1, for recursive binarySearch
+            int updatedLength = binarySearch(1, length, indices, a, a[i]);
+            lenStore[i] = indices[updatedLength - 1];
+            indices[updatedLength] = i;
+            length = Math.max(length, updatedLength);
+        }
+        ArrayList<Integer> sol = new ArrayList<>();
+        for (int i = indices[length]; indices[i] != Integer.MIN_VALUE; ) {
+            sol.add(0, a[i]);
+            i = indices[i];
+        }
+        return sol;
+    }
 
-		// printing the helper array
-		/*
-		 * for (int k : ans) { System.out.print(k + " "); }
-		 */
-		return length;
-	}
+    private static int binarySearch(int left, int right, int[] indices, int[] a, int x) {
+        if (left > right) {
+            return left;
+        }
+        int mid = (left + right) / 2;
+        if (a[indices[mid]] < x) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+        return binarySearch(left, right, indices, a, x);
+    }
 
 }
